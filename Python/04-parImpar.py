0929747ca601b718bb7a916fa708d7887dd0b3fd
Hurtado Méndez Arturo <arturohurtadomendez90@gmail.com>
2024-12-12 21:26:47 -0500

    4. número par o impar.

---
+++ Python/04-parImpar.py
@@ -0,0 +1,9 @@
+# 8. Programa para determinar si un número es par o impar.
+
+ingresaNumero = float(input("Ingresa un número: "))
+
+if ingresaNumero % 2 == 0:
+    print(f"El número {ingresaNumero} es par.")
+
+else:
+    print(f"El número {ingresaNumero} es impar.")
