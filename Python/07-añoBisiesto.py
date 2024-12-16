6ebfae7b0bb7d6ab7a22c259f6a535b3e5a7e148
Hurtado Méndez Arturo <arturohurtadomendez90@gmail.com>
2024-12-15 20:16:43 -0500

    6. año bisiesto.

---
+++ Python/07-añoBisiesto.py
@@ -0,0 +1,8 @@
+# 14. Determinar si un año es bisiesto.
+
+ingreseYear = int(input("Ingrese un año: "))
+if (ingreseYear  % 4 == 0) and ( ingreseYear % 100 != 0 or ingreseYear % 400 == 0):
+    print(f"El año {ingreseYear} es bisiesto.")
+
+else:
+    print(f"El año {ingreseYear} no es bisiesto.")
