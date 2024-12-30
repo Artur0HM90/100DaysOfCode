2b11fcd33e5d41d1174755105fe756e5a78bf413
Hurtado Méndez Arturo <arturohurtadomendez90@gmail.com>
2024-12-30 16:54:28 -0500

    19. Cronómetro.

--- Python/19-cronometro.py
+++ Python/19-cronometro.py
@@ -10,13 +10,15 @@ input("Presione 'Enter' para comenzar.")
 inicio = time.time()
 print("Cronómetro iniciando...")
 
-input("\nPresione 'Enter' para detener.")
-fin = time.time()
+print("Presiona 'Ctrl+C' para detenerlo.")
 
-transcurrido = fin - inicio
-
-horas = int(transcurrido // 3600)
-minutos = int((transcurrido % 3600) // 60)
-segundos = transcurrido % 60
-
-print(f"Tiempo transcurrido: {horas} horas, {minutos} minutos y {segundos:2F} segundos.")
+try:
+    while True:
+        transcurrido = time.time() - inicio
+        horas = int(transcurrido // 3600)
+        minutos = int((transcurrido % 3600) // 60)
+        segundos = transcurrido % 60
+        print(f"Tiempo transcurrido: {horas} horas, {minutos} minutos y {segundos:.2f} segundos.", end="\r")
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("\nCronómetro detenido.")
